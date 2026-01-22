96a32600ccc33d73602674cb15b5455a0247060d
Kamyabali1 <kamyabali000@gmail.com>
2026-01-22 15:32:38 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical chatbot",
+    version = "0.1.0",
+    author = "Kamyabali Agharia",
+    author_email="kamyabali000@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
